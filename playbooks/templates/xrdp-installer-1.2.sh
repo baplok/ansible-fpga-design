15467af03d67dab4e545a6b7771e6668e3e2f744
Tafintsev Konstantin <tafkos@mail.ru>
2020-06-15 20:17:16 +0300

    fixed sound and icons disapeared on desktop issues then connecting by xRDP

--- playbooks/templates/xrdp-installer-1.2.sh
+++ playbooks/templates/xrdp-installer-1.2.sh
@@ -590,15 +590,15 @@ if [[ $EUID -ne 0 ]]; then
 else
 	echo
 	/bin/echo -e "\e[1;31m   !-------------------------------------------------------------!\e[0m"
-	/bin/echo -e "\e[1;31m   !  Script launched with sudo command. Script will not run...  !\e[0m"
+	/bin/echo -e "\e[1;31m   !  Script launched with sudo command. Script will run...      !\e[0m"
 	/bin/echo -e "\e[1;31m   !  Run script a standard user account (no sudo). When needed  !\e[0m"
 	/bin/echo -e "\e[1;31m   !  script will be prompted for password during execution      !\e[0m"
 	/bin/echo -e "\e[1;31m   !                                                             !\e[0m"
-	/bin/echo -e "\e[1;31m   !  Exiting Script - No Install Performed !!!                  !\e[0m"
+#	/bin/echo -e "\e[1;31m   !  Exiting Script - No Install Performed !!!                  !\e[0m"
 	/bin/echo -e "\e[1;31m   !-------------------------------------------------------------!\e[0m"
 	echo
 	#sh_credits
-	exit
+	#exit
 fi
 
 #---------------------------------------------------#
